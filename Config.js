67bb114e95761364449bd68276c2506441526bf1
AlbinDeploy <albinpremam12@gmail.com>
2026-05-29 09:45:43 -0700

    Add files via upload

---
+++ Config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // ─── SERVER ──────────────────────────────────────────────────────────────
+    server_name: "Growtopia",
+    // public ipv4
+    server_ip: "143.198.211.253",
+    // UDP port
+    server_port: 55000,
+    // login url dashboard
+    loginurl: "growtopia-login-backend-omega-seven.vercel.app",
+    // for new server header
+    type2: true,
+    // meta
+    meta: "-",
+    storj_enabled: true,
+    storj_cdn_url: "https://raw.githubusercontent.com/AlbinDeploy/osm/main/public/cache",
+    storj_cache_local: true,
+    storj_timeout: 15000,
+};
